77df0f503e84afa31ff683b4985864f9533ab622
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-14 17:52:54 +0600

    Types and interfaces for carousel

---
+++ src/components/Carousel/types.ts
@@ -0,0 +1,24 @@
+import {CSSProperties, ReactChild} from "react";
+
+export interface StyleInterface {
+    [key: string]: CSSProperties
+}
+
+export interface SlideNumberOption {
+    xxl?: number,
+    xl?: number,
+    lg?: number,
+    md?: number,
+    sm?: number,
+    xs?: number,
+    base: number,
+}
+
+export interface CarouselProps {
+    children: ReactChild[],
+    slidesInViewport?: number | SlideNumberOption,
+    gap?: boolean,
+    onLastSlide?: any,
+    infinite?: boolean,
+    async?: boolean,
+}
