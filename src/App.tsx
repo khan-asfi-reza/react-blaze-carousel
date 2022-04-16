44e507f16547c344968be03aff8e751cb9a9d5c1
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-16 17:07:23 +0600

    App Testing fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useRef, useState} from 'react';
 import Carousel from "./components/Carousel";
 
-const wait = (ms: number) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+
 
 function App() {
+    const wait = (ms: number) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
     const [list, setList] = useState([
         {
             id: 1,
@@ -19,62 +21,34 @@ function App() {
             text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! " +
                 "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita " +
                 "maxime omnis pariatur placeat qui quo repudiandae.",
-            color: "crimson"
-        },
-        {
-            id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! Lorem ipsum " +
-                "dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita maxime omnis " +
-                "pariatur placeat qui quo repudiandae.",
-            color: "orange"
-        },
-        {
-            id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! Lorem ipsum " +
-                "dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita maxime omnis " +
-                "pariatur placeat qui quo repudiandae.",
             color: "purple"
         },
         {
             id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! Lorem ipsum " +
-                "dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita " +
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! " +
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita " +
                 "maxime omnis pariatur placeat qui quo repudiandae.",
-            color: "red"
+            color: "yellowgreen"
         },
         {
             id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! Lorem ipsum " +
-                "dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita maxime omnis pariatur " +
-                "placeat " +
-                "qui quo repudiandae.",
-            color: "black"
-        },
-        {
-            id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! Lorem ipsum " +
-                "dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita maxime omnis pariatur " +
-                "placeat " +
-                "qui quo repudiandae.",
-            color: "wheat"
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, explicabo! " +
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor enim error est expedita " +
+                "maxime omnis pariatur placeat qui quo repudiandae.",
+            color: "crimson"
         },
     ]);
 
 
     return (
         <div className="container mx-auto py-32">
-            <Carousel  gap slidesInViewport={{
-                base: 1,
-                md: 2,
-                xl: 3,
-                xxl: 4,
-            }} infinite
+            <Carousel gap  slidesInViewport={2} autoplay
             >{
                     list.map((each, key) => (
                         <div id={`carousel_${key}`} style={{flexFlow: "column",
                             background: each.color, color: 'white', fontFamily: "sans-serif", padding: "2rem 1.5rem"}} key={key}>
                             <h1 >
-                                Flex box
+                                Box {key}
                             </h1>
                             <p >
                                 This my text box, welcome to my text box, {each.text} ,
